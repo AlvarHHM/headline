c89495b17ea384aa8b70163f5c2447520566d1b5
Adam C. Baker <adam.durandal@gmail.com>
2013-10-24 15:42:58 -0700

    restore search

--- public/javascripts/src/app/controllers/Search.js
+++ public/javascripts/src/app/controllers/Search.js
@@ -1,39 +1,9 @@
 define(['Palace'], function(Palace) {
   return function(view, args){
-    var results = [];
-    Palace.expose();
-    //+ populateTable :: [Result] -> Table
-    var populateTable = compose(append('table', view), map(render_('ResultRow')))
-
-    //+ getResults :: String -> Promise([Result])
-      , getResults = Http.get('/headlines')
-      //K([
-          //{id: 1, title: 'one', images: images1},
-          //{id: 2, title: 'two', images: images2}
-        //])
-
-    //+ cacheResults :: [Result] -> [Result]
-      , cacheResults = function(xs) { return results = xs; }
-
-    //+ makeResults :: Post -> AddView(Table)
-      , makeResults = compose(updateHtml('#main'), populateTable, cacheResults)
-
-    //+ init :: {term: String} -> EventStream(AddView(Table))
-      , init = compose(fmap(makeResults), getResults, pluck('term'))
-
-    //+ retrieveResult :: Id -> Result
-      , retrieveResult = function(id) {
-        return detectBy(pluck('id'), id, results);
-      }
-      , showHeadline = compose(
-            emit('render', 'Headline')
-          , retrieveResult
-          , pluck('id')
-          , pluck('currentTarget')
-        )
-      ;
-
-    fmap(init, on('submit', 'Search'))
-    fmap(showHeadline, on('click', '.row'))
+    //+ tellOthers :: Event -> Event
+    var tellOthers = function(e){return emit('submit', 'Search',
+      {term: $('#term').val()} )
+    }
+    fmap(tellOthers, on('click', '#searched'));
   };
 });
