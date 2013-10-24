98202317b82c9d7366414cc26efd6527674219be
Brian Lonsdorf <brian@looprecur.com>
2013-10-24 13:50:45 -0700

    remove Palace

--- public/javascripts/src/app/controllers/App.js
+++ public/javascripts/src/app/controllers/App.js
@@ -12,9 +12,9 @@ define([
   , Headline
 ) {
   return function() {
-    Palace.register('Headline', startController('Headline', Headline));
-    Palace.register('Search', startController('Search', Search));
-    Palace.register('Results', startController('Results', Results));
+    register('Headline', startController('Headline', Headline));
+    register('Search', startController('Search', Search));
+    register('Results', startController('Results', Results));
     // register('Headline', spawn('Palace', 'startController', ['Headline']));
   }
 });
