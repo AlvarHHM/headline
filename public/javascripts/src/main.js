dbf2f7c98c3616d9f41528f8d4e54dd409da0dc4
Brian Lonsdorf <brian@looprecur.com>
2013-11-11 13:38:51 -0800

    amd cleanup

--- public/javascripts/src/main.js
+++ public/javascripts/src/main.js
@@ -5,6 +5,7 @@ require.config( {
     , text: 'vendor/require/plugins/text'
     , Palace:  'vendor/palace/0.1/palace'
     , FlapJax: 'vendor/flapjax'
+    , Bacon: 'vendor/bacon/dist/Bacon'
     , FunctionalJS: 'vendor/FunctionalJS/functional'
     , PreludeJS: 'vendor/PreludeJS/prelude'
     , Socket: 'src/app/lib/Socket'
